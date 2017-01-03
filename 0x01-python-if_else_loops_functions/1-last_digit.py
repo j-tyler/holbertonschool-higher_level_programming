f8db81771ffd7e5bec18ac66188961ca4e9ec256
j-tyler <justin.marsh@holbertonschool.com>
2017-01-03 23:35:44 +0000

    0x01 1: PEP8 formatting

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,8 +4,11 @@ number = random.randint(-10000, 10000)
 if number % 10 == 0:
     print("Last digit of {} is 0 and is 0".format(number))
 elif number < 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, (10 - number % 10) * -1))
+    print("Last digit of {} is {}".format(number, (10 - number % 10) * -1),
+          "and is less than 6 and not 0")
 elif number % 10 < 6:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, number % 10))
+    print("Last digit of {} is {}".format(number, number % 10),
+          "and is less than 6 and not 0")
 else:
-    print("Last digit of {} is {} and is greater than 5".format(number, number % 10))
+    print("Last digit of {} is {}".format(number, number % 10),
+          "and is greater than 5")
