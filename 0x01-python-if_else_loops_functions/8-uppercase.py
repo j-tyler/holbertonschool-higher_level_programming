48974bc09a58df1499e1086fccfabfcc52a7825c
j-tyler <justin.marsh@holbertonschool.com>
2017-01-04 02:00:28 +0000

    0x01 Add: 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    l = list(str)
+    for i in range(len(l)):
+        if ord(l[i]) >= ord('a') and ord(l[i]) <= ord('z'):
+            l[i] = chr(ord(l[i]) - 32)
+    str = "".join(l)
+    print("{}".format(str))
