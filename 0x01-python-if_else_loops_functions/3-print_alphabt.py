11d6ab82d999e4627d4e5838c37dabddcba55b65
j-tyler <justin.marsh@holbertonschool.com>
2017-01-04 00:35:40 +0000

    0x01 3: Change or to and

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z') + 1):
-    if i != ord('q') or i != ord('e'):
+    if i != ord('q') and i != ord('e'):
         print("{}".format(chr(i)), end="")
