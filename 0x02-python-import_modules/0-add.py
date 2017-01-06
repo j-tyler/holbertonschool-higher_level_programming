4bc72303a997044e9b8a274500c8fff6882ff814
j-tyler <justin.marsh@holbertonschool.com>
2017-01-06 06:02:18 +0000

    0x02 0: Changing for checker specification

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def main():
-    from add_0 import add
-    a, b = 1, 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 if __name__ == "__main__":
-    main()
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+
