14fcdf2dd264c04a2ef0f12668059658356227db
j-tyler <justin.marsh@holbertonschool.com>
2017-01-14 07:13:22 +0000

    0x05 0: remove space around print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,10 +3,10 @@ def safe_print_list(my_list=[], x=0):
     c = 0
     try:
         while c < x:
-            print ('{}'.format(my_list[c]), end="")
+            print('{}'.format(my_list[c]), end="")
             c += 1
     except IndexError:
-        print ()
+        print()
         return c
-    print ()
+    print()
     return c
