3ea7a60ae3d040bf9d42b47022c245e33e0feb3d
j-tyler <justin.marsh@holbertonschool.com>
2017-01-14 07:31:52 +0000

    Playing games with Mr. G's checkers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     c = 0
     for i in my_list:
         try:
-            print('{:d}'.format(i), end="")
+            print("{:d}".format(i), end="")
             c = c + 1
             if c == x:
                 break
