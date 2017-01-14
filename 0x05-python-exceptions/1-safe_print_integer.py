59d2426c23d4efa51d53b32c82fe6f39d909ba17
j-tyler <justin.marsh@holbertonschool.com>
2017-01-14 07:25:09 +0000

    testing

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,6 @@
 def safe_print_integer(value):
     try:
         print('{:d}'.format(value))
-    except (TypeError, ValueError):
-        return False
-    else:
         return True
+    except:
+        return False
