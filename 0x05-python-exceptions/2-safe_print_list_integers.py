765148845855abe869f7e225007abc7e02c1d20f
j-tyler <justin.marsh@holbertonschool.com>
2017-01-14 07:09:01 +0000

    0x05 2: Rewrite

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        c = 0
-        while c < x:
-            print('{:d}'.format(my_list[c]), end="")
-            c += 1
-    except (IndexError, ValueError):
-        print()
-        return c
+    c = 0
+    for i in my_list:
+        try:
+            print('{:d}'.format(i), end="")
+            c = c + 1
+            if c == x:
+                break
+        except (ValueError, TypeError):
+            continue
     print()
     return c
