9f534ee0bb4e92e3c774c2fd73fcd76df7660dea
j-tyler <justin.marsh@holbertonschool.com>
2017-01-19 06:10:25 +0000

    0x06 Add: 4-print_square.py

---
+++ 0x06-python-test_driven_development/4-print_square.py
@@ -0,0 +1,24 @@
+"""
+This module prints out a poundsymbol square
+There is one function, print_square(size)
+
+>>> print_square(3)
+###
+###
+###
+"""
+
+
+def print_square(size):
+    """
+    prints out a square with pound symbols
+    """
+    if not isinstance(size, (int, float)):
+        raise TypeError("size must be an integer")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    s = '#' * size + '\n'
+    s = s * size
+    print("{:s}".format(s), end="")
