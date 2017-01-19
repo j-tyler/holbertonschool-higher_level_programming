2065afb2db5dd55b5aeb8b9e83813147b2cfd3d7
j-tyler <justin.marsh@holbertonschool.com>
2017-01-19 06:22:06 +0000

    0x06 4: Testing alternate solution

--- 0x06-python-test_driven_development/4-print_square.py
+++ 0x06-python-test_driven_development/4-print_square.py
@@ -14,9 +14,7 @@ def print_square(size):
     """
     prints out a square with pound symbols
     """
-    if not isinstance(size, (int, float)):
-        raise TypeError("size must be an integer")
-    if isinstance(size, float) and size < 0:
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
