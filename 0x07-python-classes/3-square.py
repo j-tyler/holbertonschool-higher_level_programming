bda4f2960c8fc277fe05c0ca5fb08d7fac6356c0
j-tyler <justin.marsh@holbertonschool.com>
2017-01-23 22:30:33 +0000

    0x07 Add: 4-square.py

--- 0x07-python-classes/3-square.py
+++ 0x07-python-classes/3-square.py
@@ -8,4 +8,4 @@ class Square:
         self.__size = size
 
     def area(self):
-        return size * size
+        return self.__size * self.__size
--- 0x07-python-classes/4-square.py
+++ 0x07-python-classes/4-square.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size=0):
+        self.size = size;
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        _size = size
+
+    def area(self):
+        return self.__size * self.__size
