54a176a2ba3957d1c5e5f23bf3de6c68f1532cf6
j-tyler <justin.marsh@holbertonschool.com>
2017-01-23 23:13:33 +0000

    0x07 Add: 5-square.py

---
+++ 0x07-python-classes/5-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        print('{}'.format('\n'.join(["#" * self.__size
+              for i in range(0, self.__size)])))
