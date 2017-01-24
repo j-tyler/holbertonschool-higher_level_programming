bb6679cb7fb89f503f7dce4cf15c591e1fffaa06
j-tyler <justin.marsh@holbertonschool.com>
2017-01-24 07:45:54 +0000

    0x07 Add: 101-square.py

---
+++ 0x07-python-classes/101-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+next step in square class
+"""
+
+
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    def __str__(self):
+            string = "{}".format('\n' * self.__position[1]) + "{}".format('\n'.join([" " * self.__position[0] + "#" * self.__size for i in range(0, self.__size)]))
+            return string
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        print('{}'.format('\n' * self.__position[1]), end="")
+        print('{}'.format('\n'.join([" " * self.__position[0] +
+              "#" * self.__size for i in range(0, self.__size)])))
