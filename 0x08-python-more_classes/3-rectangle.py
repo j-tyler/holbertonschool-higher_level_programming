58b82ba9fdf4e2672b6635017170de550b34f994
j-tyler <justin.marsh@holbertonschool.com>
2017-01-25 07:30:13 +0000

    rename var

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -33,9 +33,9 @@ class Rectangle:
     @height.setter
     def height(self, height):
         if not isinstance(height, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if height < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = height
 
     def area(self):
