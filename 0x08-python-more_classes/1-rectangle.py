c2d446c8039555c574a4b1efa493e419c9443407
j-tyler <justin.marsh@holbertonschool.com>
2017-01-25 07:24:49 +0000

    0x08 1: Add argument to height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -26,9 +26,9 @@ class Rectangle:
         self.__width = width
 
     @height.setter
-    def height(self):
-        if not isinstance(width, int):
+    def height(self, height):
+        if not isinstance(height, int):
             raise TypeError("width must be an integer")
-        if width < 0:
+        if height < 0:
             raise ValueError("width must be >= 0")
         self.__height = height
