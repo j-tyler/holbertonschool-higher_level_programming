769bfd15bcad291004d6f0c5d49ba4d1b170509b
j-tyler <justin.marsh@holbertonschool.com>
2017-01-25 07:32:38 +0000

    change error message

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -32,9 +32,9 @@ class Rectangle:
     @width.setter
     def width(self, width):
         if not isinstance(width, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if width < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__width = width
 
     @height.setter
