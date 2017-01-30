75affc0aa0527bbd692568f39d99a1bbf43bdb02
j-tyler <justin.marsh@holbertonschool.com>
2017-01-30 19:04:37 +0000

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+replicate isinstanceof
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    replicate isinstanceof
+    """
+    if isinstance(obj, a_class):
+        return True
