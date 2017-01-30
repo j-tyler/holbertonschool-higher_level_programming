cc5490b439b3f28bd8f4152dfb5f5cfe22aa52cc
j-tyler <justin.marsh@holbertonschool.com>
2017-01-30 19:11:53 +0000

    0x0A Add: 4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+replicate issubclass
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    replicate issubclass
+    """
+    if issubclass(type(obj), a_class):
+        if type(obj) != a_class:
+            return True
