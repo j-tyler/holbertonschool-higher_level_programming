70d7be96edf2f7115c5c863020f6a542c7db927a
j-tyler <justin.marsh@holbertonschool.com>
2017-02-01 06:17:08 +0000

    Add false return statement

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -10,3 +10,4 @@ def is_kind_of_class(obj, a_class):
     """
     if isinstance(obj, a_class):
         return True
+    return False
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -11,3 +11,4 @@ def inherits_from(obj, a_class):
     if issubclass(type(obj), a_class):
         if type(obj) != a_class:
             return True
+    return False
