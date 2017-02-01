2a06cf4ae2a82fd169de1e0d85d9874445da884b
j-tyler <justin.marsh@holbertonschool.com>
2017-02-01 02:25:20 +0000

    Indent docstring

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,7 +9,7 @@ class MyList(list):
     duplicate list method of sorting
     """
     def print_sorted(self):
-    """
-    print the list sorted
-    """
+        """
+        print the list sorted
+        """
         print("{}".format(sorted(self)))
