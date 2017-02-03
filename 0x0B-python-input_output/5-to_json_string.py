2dfba105a572e91353d36a40a735ac62054ffccd
j-tyler <justin.marsh@holbertonschool.com>
2017-02-03 01:44:54 +0000

    0x0B Add: 5-to-json_string.py

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module includes to_json_string
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Returns a string representation of an object
+    """
+    return json.dumps(my_obj)
