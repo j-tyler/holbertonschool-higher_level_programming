2c03590bbf7a5147808c95f62142e980be750c74
j-tyler <justin.marsh@holbertonschool.com>
2017-02-28 22:13:22 +0000

    0x0D Add 7-cities.sql

---
+++ 0x0D-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- Create database hbtn_0d_usa and table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa; 
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
+(id INT NOT NULL UNIQUE AUTO_INCREMENT,
+ name VARCHAR(256) NOT NULL,
+ state_id INT NOT NULL,
+ FOREIGN KEY (state_id)
+ REFERENCES hbtn_0d_usa.states(id)
+);
