1f96ba5e7862300bfe4ecf8d939c136c1b42b03d
j-tyler <justin.marsh@holbertonschool.com>
2017-03-09 00:24:27 +0000

    Do not execute on import

--- 0x0E-python-object_relational_mapping/1-filter_states.py
+++ 0x0E-python-object_relational_mapping/1-filter_states.py
@@ -4,15 +4,16 @@ lists all states starting with N
 """
 
 
-import MySQLdb
-import sys
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
-db = MySQLdb.connect(host='localhost', user=sys.argv[1],
-                     passwd=sys.argv[2], db=sys.argv[3])
-cur = db.cursor()
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
 
-cur.execute("SELECT id, name FROM states")
-rows = cur.fetchall()
-for row in rows:
-    if (row[1][0] == 'N'):
-        print("{}".format(row))
+    cur.execute("SELECT id, name FROM states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        if (row[1][0] == 'N'):
+            print("{}".format(row))
