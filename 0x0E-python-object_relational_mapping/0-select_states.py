10840962bd479fc02779ccbbe2f3a99420ab35f1
j-tyler <justin.marsh@holbertonschool.com>
2017-03-09 00:20:03 +0000

    Make sure query is sorted

--- 0x0E-python-object_relational_mapping/0-select_states.py
+++ 0x0E-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                      passwd=sys.argv[2], db=sys.argv[3])
 cur = db.cursor()
 
-cur.execute("SELECT id, name FROM states")
+cur.execute("SELECT id, name FROM states ORDER BY states.id ASC")
 rows = cur.fetchall()
 for row in rows:
     print("{}".format(row))
