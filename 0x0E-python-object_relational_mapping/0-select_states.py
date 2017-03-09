ada2703d45f6e944703aa89909439283df2baeb1
j-tyler <justin.marsh@holbertonschool.com>
2017-03-09 00:12:11 +0000

    Add 0-select_states.py

---
+++ 0x0E-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Grab all states from database hbtn_0e_0_usa
+"""
+
+
+import MySQLdb
+import sys
+
+db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                     passwd=sys.argv[2], db=sys.argv[3])
+cur = db.cursor()
+
+cur.execute("SELECT id, name FROM states")
+rows = cur.fetchall()
+for row in rows:
+    print("{}".format(row))
