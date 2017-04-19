6a409248b9c39a5422a08d496f771befabee8456
Justin Marsh <jumarsh@linkedin.com>
2017-04-19 16:17:42 -0700

    Finish Rectangle implimentation, sans toString

--- 0x08-python-more_classes/Rectangle.java
+++ 0x08-python-more_classes/Rectangle.java
@@ -1,33 +1,67 @@
 public class Rectangle
 {
-	public static void Rectangle
-	{
-		private int width;
-		private int height;
+	private int width;
+	private int height;
+	private char symbol = '#';
 
-		public Rectangle() {
-			this.width = 0;
-			this.height = 0;
+	public Rectangle() {
+		this.width = 0;
+		this.height = 0;
+	}
+
+	public Rectangle(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+
+	@Override
+	public String toString() {
+		//TODO: Visualize the square with some string object
+		return "Rectangle Object";
+	}
+
+	public void setWidth(int width) {
+		if (width < 0) {
+			throw new IndexOutOfBoundsException;
 		}
+		this.width = width;
+	}
 
-		public Rectangle(int width, int height) {
-			this.width = width;
-			this.height = height;
+	public void setHeight(int height) {
+		if (height < 0) {
+			throw new IndexOutOfBoundsException;
 		}
+		this.width = width;
+	}
 
-		public setWidth(int width) {
-			if (width < 0) {
-				throw new IndexOutOfBoundsException;
-			}
-			this.width = width;
+	public void setSymbol(char symbol) {
+		this.symbol = symbol;
+	}
+
+	public int area() {
+		return this.width * this.height;
+	}
+
+	public int perimeter() {
+		if (this.width == 0 || this.height == 0) {
+			return 0;
 		}
+		return this.width * 2 + this.height * 2;
+	} 
 
-		public setHeight(int height) {
-			if (height < 0) {
-				throw new IndexOutOfBoundsException;
-			}
-			this.width = width;
+	public static boolean isBigger(Rectangle one, Rectangle, two) {
+		if (one > two) {
+			return true;
+		} else {
+			return false;
 		}
+	}
 
+	public static Rectangle square(int size) {
+		if (size < 0) {
+			throw new IndexOutOfBoundsException;
+		}
+		return new Rectangle(size, size);
 	}
 }
+	
